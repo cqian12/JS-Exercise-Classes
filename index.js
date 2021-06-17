90dd660a759ea045d7ff916ae465d3b36b974741
Charles Qian <charleshqian@gmail.com>
2021-06-17 13:33:56 -0400

    completed task 1

--- index.js
+++ index.js
@@ -42,7 +42,19 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age) {
+      this.name = name
+      this.age = age
+      this.stomach = []
+    }
+    eat(someFood) {
+      if (this.stomach.length < 10) {
+        this.stomach.push(someFood)
+      }
+    }
+    toString() {
+      return `${this.name}, ${this.age}`
+    }
   }
   
   /*
